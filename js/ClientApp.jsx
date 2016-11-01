69e25df77c7bc965944f839ef619c80fbe769d50
Ian Thorp <IanMThorp@gmail.com>
2016-11-01 14:03:58 -0700

    Add search function

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -5,13 +5,17 @@ const ReactRouter = require('react-router')
 const Router = ReactRouter.Router
 const Route = ReactRouter.Route
 const hashHistory = ReactRouter.hashHistory
+const IndexRoute = ReactRouter.IndexRoute
 const Search = require('./Search')
 // const { Router, Route, hashHistory } = require('react-router')
+const Layout = require('./Layout')
 
 const App = () => (
   <Router history={hashHistory}>
-    <Route path='/' component={Landing} />
-    <Route path='/search' component={Search} />
+    <Route path='/' component={Layout}>
+      <IndexRoute component={Landing} />
+      <Route path='/search' component={Search} />
+    </Route>
   </Router>
 )
 
