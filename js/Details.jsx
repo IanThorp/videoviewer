f40200527211519e4902d5c084f33adca32bbafb
Ian Thorp <IanMThorp@gmail.com>
2016-11-01 17:33:39 -0700

    Add details page

---
+++ js/Details.jsx
@@ -0,0 +1,19 @@
+const React = require('react')
+
+class Details extends React.Component {
+  render () {
+    return (
+      <div className='container'>
+        <pre><code>
+          {JSON.stringify(this.props.params, null, 4)}
+        </code></pre>
+      </div>
+    )
+  }
+}
+
+Details.propTypes = {
+  params: React.PropTypes.object.isRequired
+}
+
+module.exports = Details
