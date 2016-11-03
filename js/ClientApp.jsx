680d293c838d9054a99abf6850e58b6702070787
Ian Thorp <IanMThorp@gmail.com>
2016-11-03 16:58:57 -0700

    Enable webpack chunking

--- js/ClientApp.jsx
+++ js/ClientApp.jsx
@@ -1,26 +1,56 @@
 const React = require('react')
-const Landing = require('./Landing')
-const { Router, Route, hashHistory, IndexRoute } = require('react-router')
-const Search = require('./Search')
-const Details = require('./Details')
+const { Router, browserHistory } = require('react-router')
 const Layout = require('./Layout')
 const { store } = require('./Store')
 const { Provider } = require('react-redux')
+
+if (typeof module !== 'undefined' && module.require) {
+  if (typeof require.ensure === 'undefined') {
+    require.ensure = require('node-ensure')
+  }
+}
+
+const rootRoute = {
+  component: Layout,
+  path: '/',
+  indexRoute: {
+    getComponent (location, cb) {
+      require.ensure([], () => {
+        cb(null, require('./Landing'))
+      })
+    }
+  },
+  childRoutes: [
+    {
+      path: 'search',
+      getComponent (location, cb) {
+        require.ensure([], () => {
+          cb(null, require('./Search'))
+        })
+      }
+    },
+    {
+      path: 'details/:id',
+      getComponent (location, cb) {
+        require.ensure([], () => {
+          cb(null, require('./Details'))
+        })
+      }
+    }
+  ]
+}
 
 const App = React.createClass({
   render () {
     return (
       <Provider store={store}>
-        <Router history={hashHistory}>
-          <Route path='/' component={Layout}>
-            <IndexRoute component={Landing} />
-            <Route path='/search' component={Search} />
-            <Route path='/details/:id' component={Details} />
-          </Route>
-        </Router>
+        <Router history={browserHistory} routes={rootRoute} />
       </Provider>
     )
   }
 })
+
+App.Routes = rootRoute
+App.History = browserHistory
 
 module.exports = App
